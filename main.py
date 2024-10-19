c9e840de18c760da1a6e0dfabfb740ad6149abe0
student <student@itstep1.dp.ua>
2024-10-19 18:07:59 +0300

    created class Marker

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Hello world")
+
+a = "Text"
+b = 2
+c = 'hello'
+d = True
+e = 2.2
+
+
+class Marker:
+    def __int__(self):
+        self.weight = 13
+        self.price = 5.22
+        self.isPresent = True
+
+
+myMarket = Marker()
+print(myMarket.price)
